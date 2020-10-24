59b62b7515997edc49fa1dfc4fdef376aebd6394
in kyungil <propose98@naver.com>
2020-10-24 12:56:27 +0900

    실습2

--- main.go
+++ main.go
@@ -1,17 +1,6 @@
 package main
 
-import "fmt"
-
-func main()  {
-	bc := NewBlockchain()
-
-	bc.AddBlock("send 1 Token to KKK")
-	bc.AddBlock("send 1 Token to TEMP")
-
-	for _, block := range bc.blocks {
-		fmt.Printf("Prev. Hash: %x\n", block.PrevBlockHash)
-		fmt.Printf("Data: %s\n", block.Data)
-		fmt.Printf("Hash: %x\n", block.Hash)
-		fmt.Println()
-	}
+func main() {
+	cli := CLI{}
+	cli.Run()
 }
